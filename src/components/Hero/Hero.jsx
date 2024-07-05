742a5fcfb8b2a0868b0ef98a7cb185bdcb5ef0c3
akhilaae <akkupktn@gmail.com>
2024-07-05 19:43:59 +0530

    skill_update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ export const Hero = () => {
   return <section className={styles.container}>
     <div className={styles.content}>
         <h1 className={styles.title}>Hi! I am Akhila</h1>
-        <p className={styles.description}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem hic voluptatum corporis ad ex? Possimus placeat impedit, corporis consectetur</p>
+        <p className={styles.description}>Passionate about Technology and Web-Development with a strong enthusiasm for creating engaging user experiences. Unwavering commitment to continuous skill enhancement and learning.</p>
         <a className={styles.contactBtn} href="mailto:akhilap0307@gmail.com">Contact me</a>
     </div>
     <img className={styles.heroImg}src={getImageUrl("profile.png")} alt="profile" />
